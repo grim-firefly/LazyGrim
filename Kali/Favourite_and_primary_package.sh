a8618a69d40547926a65e361bbf465781028a61f
Grim Firefly <56791251+grim-firefly@users.noreply.github.com>
2020-12-03 00:25:42 +0600

    addes more script and added vscode extension
    vscode extension will automatically install and vscode , auto customize, auto download some basic packages , create coding environment

--- Kali/Favourite_and_primary_package.sh
+++ Kali/Favourite_and_primary_package.sh
@@ -5,7 +5,7 @@
 #!/bin/bash
 
 #initializing some shortcut
-spg="sudo apt-get"
+spg="sudo apt"
 spgi="$spg install"
 scl="\033[31;01;04m"
 ecl="\033[0m"
@@ -16,32 +16,65 @@ si="Successfully Installed "
 
 #this is for primary package like gcc,g++,fonts and so on
 
-function primary_package() {
+primary_package() {
     $spg update
-    $spgi -y wget
-    $spgi -y build-essential
+    $spgi -y wget software-properties-common apt-transport-https
+    $spgi -y build-essential 
     $spgi -y vim git gparted
     $spgi -y alacarte plank gnome-tweak-tool
+    sudo add-apt-repository ppa:agornostal/ulauncher -y
+    $spgi -y ulauncher
 }
 #browser like google chrome and firefox
-function browser_ins() {
-   
-}
-#for installing ide for programming
-function ide_editor() {
-    #install codeblocks
-    $spgi -y codeblocks python3
 
+#for installing ide for programming
+ide_editor() {
+    #install codeblocks
+    $spgi -y codeblocks python3 python2
+    #install vscode
+    wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" -y
+    $spg update
+    sudo apt install code -y
+    #vscode extension
+    code --install-extension coenraads.bracket-pair-colorizer
+    code --install-extension ms-vscode.cpptools
+    code --install-extension formulahendry.code-runner
+    code --install-extension divyanshuagrawal.competitive-programming-helper
+    code --install-extension darktears.dark-c-plus-plus-theme
+    code --install-extension vscodevim.vim
 }
 #for installing media player
-function media_player() {
+media_player() {
     #rhythmbox
     $spgi -y rhythmbox
     #vlc player
     $spgi -y vlc
 }
+#Browser
+download_and_customize(){
+	mkdir .icons .themes
+	cd .local/share/
+	mkdir plank
+	cd plank
+	mkdir themes
+	cd ~
+	cd Downloads
+	wget -O chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+	wget -O xdm.tar.xz https://github.com/subhra74/xdm/releases/download/7.2.11/xdm-setup-7.2.11.tar.xz
+	tar -xvf xdm.tar.xz
+	sudo ./install.sh
+	rm install.sh readme.txt
+	wget -O macos.tar.xz https://github.com/grim-firefly/LazyGrim/releases/download/Themes_1/themes.tar.xz
+	tar -xvf macos.tar.xz
+	mv whitesur_cursors lacapatina ~/.icons
+	mv whitesur_alt ~/.themes
+	mv cataline_white ~/.local/share/plank/themes
+	cd ~
+	
+}
 #some folder and file creation
-function directory_needed_by_me() {
+directory_needed_by_me() {
     mkdir Hacking Programming   GIT
     cd Programming
     mkdir Atcoder  CodeForces   HackerRank  LightOJ   UVA Vjudge Others A2OJ Sheet
@@ -50,21 +83,21 @@ function directory_needed_by_me() {
     cd ../
     cd Atcoder
     mkdir Today Virtual Upsolve Others
-
+    cd ~
 }
 echo -e "*****$scl Scripted By $dbg $ecl*****"
 echo -e "$scl $ins primary package $ecl"
 primary_package
 echo -e "$scl Primary Packages Are $si $ecl"
-echo -e "$scl $ins Browsers $ecl"
-browser_ins
-echo -e "$scl Browsers $si $ecl"
 echo -e "$scl codeblocks and vscode $ins $ecl"
 ide_editor
 echo -e "$scl codeblocks and vscode $si $ecl"
 echo -e "$scl $ins media player vlc and rhythmbox $ecl"
 media_player
 echo -e "$scl $si rhythmbox and vlc $ecl"
+echo -e "$scl download some basic things and customize $ecl"
+download_and_customize
+echo -e "$scl downloaded and customized successfull $ecl"
 echo -e "$scl Creating Directory :) $ecl"
 directory_needed_by_me
 echo -e "$scl done creating Directory and files :) $ecl"
